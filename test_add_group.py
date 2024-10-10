531549461248d5bf4a11fc634cf0c808d49aab60
eushko <eushko@pikemedia.ru>
2024-10-11 01:22:28 +0300

    Тест на добавление группы исправлен для запуска в FF

--- test_add_group.py
+++ test_add_group.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from selenium.webdriver.firefox.options import Options
 
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        options = Options()
+        options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+        self.wd = webdriver.Firefox(executable_path=r'C:\Windows\System32\geckodriver.exe', options=options)
         self.wd.implicitly_wait(30)
-    
+
     def test_untitled_test_case(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
@@ -39,19 +42,25 @@ class UntitledTestCase(unittest.TestCase):
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_link_text("Logout").click()
-    
+
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
+
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
