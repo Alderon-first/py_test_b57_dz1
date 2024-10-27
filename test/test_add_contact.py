babbea0e6c0ec29c2c13acbf38163b7aa976e465
eushko <eushko@pikemedia.ru>
2024-10-27 23:35:35 +0300

    Лекция 2. Исправлен тест на создание группы: исправлен вызов метода создания контакта (ранее неправильно указывался адрес)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 def test_create_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="Katerina", last_name="Ushko", address= "MSK, Lasareva, 66-66",
-                                    phone_m="8-977-89-96-13", email="test@test.ru"))
+    app.contact.create(Contact(first_name="Katerina", last_name="Ushko", address="MSK, Lasareva, 66-66",
+                               phone_m="8-977-89-96-13", email="test@test.ru"))
     app.session.logout()
 
