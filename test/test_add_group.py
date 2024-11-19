10027948cdfbdb37af91cb26bdf968cc8cdbda9b
eushko <eushko@pikemedia.ru>
2024-11-19 16:36:57 +0300

    Лекция 5. Задание 15 пример параметризации из лекции: создание группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
+import pytest
 from model.group import Group
+import random
+import string
 
 
-def test_add_group(app):
+def ramdom_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits+string.punctuation+' '*10
+    return prefix+"".join((random.choice(symbols) for i in range(random.randrange(maxlen))))
+
+
+testdata = [
+    Group(name="", header="", footer="")] + \
+           [
+    Group(name=ramdom_string("name", 10), header=ramdom_string("header", 20), footer=ramdom_string("footer", 20))
+               for i in range(5)
+]
+# вариант тестдаты с перебором комбинаций
+# testdata = [
+#     Group(name="name", header=header, footer=footer)
+#     for name in ["", ramdom_string("name", 10)]
+#     for header in ["", ramdom_string("header", 20)]
+#     for footer in ["", ramdom_string("footer", 20)]
+# ]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[str(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="test_g", header="head", footer="footer")
     app.group.create(group)
     # сравниваем длинну старого списка с количеством групп на странице
     assert len(old_groups)+1 == app.group.count()
@@ -15,13 +38,3 @@ def test_add_group(app):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-# def test_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="", header="", footer="")
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-    
