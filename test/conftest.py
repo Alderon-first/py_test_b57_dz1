017bc03bd1bd79a0dbeb34a250f880a32a15f803
eushko <eushko@pikemedia.ru>
2024-11-22 14:32:21 +0300

    Лекция 6. Задание 17/1 описано установление пути к файлу target. Но, поскольку target и conftest лежат на разных уровнях, пришлось использовать не такую онструкцию, как в лекции

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,11 @@
 from fixture.application import Application
 import pytest
 import json
+import os.path
 
 fixture = None
 target = None
+
 
 @pytest.fixture
 def app(request):
@@ -11,8 +13,12 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        parent = os.path.join(os.path.dirname(__file__), os.pardir) # ищу путь к файлу и поднимаюсь к родительской директории (но путь не универсален для разных ОС)
+        # print(parent)
+        config_file = os.path.join(os.path.abspath(parent), request.config.getoption("--target")) # os.path.abspath(parent) преобразует путь так, чтобы он склеился правильным образом
+        # print(config_file)
+        with open(config_file) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target["baseUrl"])
     fixture.session.ensure_login(username=target["username"], password=target["password"])
