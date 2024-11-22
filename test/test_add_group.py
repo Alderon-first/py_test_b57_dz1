5f315cd1da902167831e9cd5e7e58875b26d22bf
eushko <eushko@pikemedia.ru>
2024-11-22 16:12:45 +0300

    Лекция 6. Задание 18 генерация данных для контакта и группы вынесена в отдельную директорию data

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,28 +1,8 @@
 # -*- coding: utf-8 -*-
 import pytest
+
+from data.add_group import testdata
 from model.group import Group
-import random
-import string
-
-
-def ramdom_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits
-    return prefix+"".join((random.choice(symbols) for i in range(random.randrange(maxlen))))
-
-
-testdata = [
-    Group(name="", header="", footer="")] + \
-           [
-    Group(name=ramdom_string("name", 10), header=ramdom_string("header", 20), footer=ramdom_string("footer", 20))
-               for i in range(5)
-]
-# вариант тестдаты с перебором комбинаций
-# testdata = [
-#     Group(name="name", header=header, footer=footer)
-#     for name in ["", ramdom_string("name", 10)]
-#     for header in ["", ramdom_string("header", 20)]
-#     for footer in ["", ramdom_string("footer", 20)]
-# ]
 
 
 @pytest.mark.parametrize("group", testdata, ids=[str(x) for x in testdata])
