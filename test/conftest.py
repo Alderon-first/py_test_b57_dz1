1dd5878c28fcac4eafca53e2b52590aac939dcca
eushko <eushko@pikemedia.ru>
2024-12-19 21:28:25 +0300

    Лекция 7. Задание 20 Попытка 1 сделать тест test_db_matches_ui.py

--- test/conftest.py
+++ test/conftest.py
@@ -13,12 +13,13 @@ target = None
 def load_config(file):
     global target
     if target is None:
-        parent = os.path.join(os.path.dirname(__file__), os.pardir) # ищу путь к файлу и поднимаюсь к родительской директории (но путь не универсален для разных ОС)
-        # print(parent)
-        config_file = os.path.join(os.path.abspath(parent), file) # os.path.abspath(parent) преобразует путь так, чтобы он склеился правильным образом
-        # print(config_file)
+        #parent = os.path.join(os.path.dirname(__file__), os.pardir) # ищу путь к файлу и поднимаюсь к родительской директории (но путь не универсален для разных ОС)
+        #print(parent)
+        config_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), file) # os.path.abspath(parent) преобразует путь так, чтобы он склеился правильным образом
+        print(config_file)
         with open(config_file) as f:
             target = json.load(f)
+            print(target)
         return target
 
 
@@ -27,6 +28,7 @@ def app(request):
     global fixture
     browser = request.config.getoption("--browser")
     web_config = load_config(request.config.getoption("--target"))["web"]
+    print(web_config)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["baseUrl"])
     fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
@@ -36,8 +38,7 @@ def app(request):
 @pytest.fixture(scope="session")
 def db(request):
     db_config = load_config(request.config.getoption("--target"))["db"]
-    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"],
-                          password=db_config["password"] )
+    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
     def fin():
         dbfixture.destroy()
     request.addfinalizer(fin)
